aacd0b2e68ef482892e2fefff9ad6d391ca26848
maxmwangikenya <maxmwangikenya@gmail.com>
2025-04-14 13:29:19 +0300

    landing page

---
+++ src/components/pageHeaderContainer/index.jsx
@@ -0,0 +1,16 @@
+import './styles.css'
+
+
+const pageHeaderContent = (props) =>{
+
+    const {headerText,icon} = props;
+
+    return(
+        <div className="wrapper">
+            <h2>{headerText}</h2>
+            <span>{icon}</span>
+        </div>
+    )
+}
+
+export default pageHeaderContent;
